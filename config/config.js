40c9552d99f060e83864ed1b663e7857833995de
prasetiodc <mprasetiodc@gmail.com>
2019-04-01 20:10:23 +0700

    muhamadPrasetio

--- config/config.js
+++ config/config.js
@@ -1,18 +1,4 @@
 let con = {
-  "development": {
-    "username": `${process.env.DB_USERNAME}`,
-    "password": `${process.env.DB_PASS}`,
-    "database": "restAPI",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
-  "test": {
-    "username": `${process.env.DB_USERNAME}`,
-    "password": `${process.env.DB_PASS}`,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
   "production": {
     "username": `${process.env.DB_USERNAME}`,
     "password": `${process.env.DB_PASS}`,
