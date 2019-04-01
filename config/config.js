cb824e35e36d0cda8a42ff9e61c4bed6b87531e2
prasetiodc <mprasetiodc@gmail.com>
2019-04-01 20:28:49 +0700

    muhamadPrasetio

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 let con = {
   "production": {
-    "username": `${process.env.DB_USERNAME}`,
-    "password": `${process.env.DB_PASS}`,
-    "database": "database_production",
+    "username": "ltkuwydbvgurlx",
+    "password": "6afa5cf87ac06fbcbdf208df40ae422cb462cb99391a536d27addb7384044188",
+    "database": "dfot86945fren7",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
 
